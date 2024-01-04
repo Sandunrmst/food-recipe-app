994232603e5a3c98ae3688eeffabb1e758de85f1
sandunrmst <sandunalugc@gmail.com>
2024-01-04 10:45:23 +0530

    create global context

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context";
 
 const Navbar = () => {
+  //Access GlobalContext values
+  const { searchParam, setSearchParam } = useContext(GlobalContext);
+  console.log(searchParam);
   return (
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0 ">
       <h2 className="text-2xl font-semibold">
@@ -17,6 +21,8 @@ const Navbar = () => {
         <input
           type="text"
           name="search"
+          value={searchParam}
+          onChange={(event) => setSearchParam(event.target.value)}
           placeholder="Enter Item..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-slate-400 focus:shadow-slate-500"
         />
