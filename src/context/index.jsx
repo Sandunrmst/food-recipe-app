20420a3ae21cc08cff54e08e808938236f547de9
sandunrmst <sandunalugc@gmail.com>
2024-01-05 12:53:43 +0530

    direction for user navigate to the home page after search anything on search bar

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
 
@@ -8,6 +9,8 @@ export default function GlobalState({ children }) {
   const [recipeList, setRecipeList] = useState([]);
   const [recipeDetailsData, setRecipeDetailsData] = useState(null);
   const [favoritesList, setFavoritesList] = useState([]);
+
+  const navigate = useNavigate();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -22,6 +25,7 @@ export default function GlobalState({ children }) {
         setRecipeList(data?.data?.recipes);
         setLoading(false);
         setSearchParam("");
+        navigate("/");
       }
 
       console.log(data);
