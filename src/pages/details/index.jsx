4e0c4a1d6eddc02805fd32d610ad2823abed1a87
sandunrmst <sandunalugc@gmail.com>
2024-01-05 11:05:41 +0530

    build details page to show single item

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../../context";
 
 const Details = () => {
-  return <div>Details</div>;
+  //Extract current ID each details page.
+  const params = useParams();
+
+  const { recipeDetailsData, setRecipeDetailsData } = useContext(GlobalContext);
+
+  console.log(params);
+
+  async function getRecipeDetails() {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${params.id}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+    if (data?.data) {
+      setRecipeDetailsData(data?.data);
+    }
+  }
+
+  useEffect(() => {
+    getRecipeDetails();
+  }, []);
+
+  return (
+    <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-flow-col-2 gap-10">
+      <div className="row-start-2 lg:row-start-auto">
+        <div className="h-96 overflow-hidden rounded-xl ">
+          <img
+            src={recipeDetailsData?.recipe?.image_url}
+            className="w-full h-full object-cover block group-hover:scale-105 duration-300"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-3">
+        <span className="text-sm text-cyan-700 font-medium">
+          {recipeDetailsData?.recipe?.publisher}
+        </span>
+        <h3 className="font-bold text-2xl truncate text-black">
+          {recipeDetailsData?.recipe?.title}
+        </h3>
+        <div>
+          <button className="p-3 px-8 rounded-lg text-sm uppercase font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white">
+            Save as Favorites
+          </button>
+        </div>
+        <div>
+          <span className="text-2xl font-semibold text-black">
+            Ingredients:{" "}
+          </span>
+          <ul className="flex flex-col gap-3">
+            {recipeDetailsData?.recipe?.ingredients.map((items) => (
+              <li>
+                <span className="text-2xl font-semibold text-black">
+                  {items.quantity} {items.unit}
+                </span>
+                <span className="text-2xl font-semibold text-black">
+                  {items.description}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
